4b50e23d4a5ef0051c9714053bc0231c35699c2e
Hanna Kulpo <anya.kulpo.1999@gmail.com>
2025-11-21 18:07:57 +0800

    Changed test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -5,28 +5,27 @@ class TestStellarBurgersConstructorForm:
     # Переход на 'Соусы'
     def test_constructor_go_to_sauces_scroll_to_sauces(self, valid_login):
         driver = valid_login
-
         driver.find_element(*MainPageLocators.MP_SAUSE_SECTION).click()
-        sauces_section = driver.find_element(*MainPageLocators.MP_SAUSE_SECTION)
 
-        assert sauces_section.text == 'Соусы'
+        active_tab = driver.find_element(By.CSS_SELECTOR, ".tab_tab_type_current__2BEPc")
+
+        assert active_tab.text == "Соусы"
 
     #  Переход на 'Начинки'
     def test_constructor_go_to_filling_scroll_to_filling(self, valid_login):
         driver = valid_login
-
         driver.find_element(*MainPageLocators.MP_FILLING_SECTION).click()
-        filling_section = driver.find_element(*MainPageLocators.MP_FILLING_SECTION)
 
-        assert filling_section.text == 'Начинки'
+        active_tab = driver.find_element(By.CSS_SELECTOR, ".tab_tab_type_current__2BEPc")
+
+        assert active_tab.text == "Начинки"
 
     #  Переход на 'Булки'
     def test_constructor_go_to_bun_scroll_to_bun(self, valid_login):
         driver = valid_login
-
         driver.find_element(*MainPageLocators.MP_FILLING_SECTION).click()
         driver.find_element(*MainPageLocators.MP_BREAD_SECTION).click()
-        bun_section = driver.find_element(*MainPageLocators.MP_BREAD_SECTION)
 
-        assert bun_section.text == 'Булки'
+        active_tab = driver.find_element(By.CSS_SELECTOR, ".tab_tab_type_current__2BEPc")
+        assert active_tab.text == "Булки"
 
